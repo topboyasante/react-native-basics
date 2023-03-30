836e65fb6794e70de266c560f0a222ff5e7ac0f9
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-03-30 10:19:22 +0000

    updates

--- assets/components/cards/WalletCard.jsx
+++ assets/components/cards/WalletCard.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import CCInner from '../../images/cc-inner.png'
 
 function WalletCard({index,name,cardNumber,cardType,expiryDate,balance}) {
-    const colors = ['#E91E63', '#2196F3', '#4CAF50', '#FFC107', '#795548'];
+    const colors = ['#E91E63', '#2196F3', '#4CAF50', '#df3512', '#3b556e'];
     
   return (
     <TouchableOpacity>
         <View style={[styles.container, { backgroundColor: colors[index % colors.length]  }]}>
             <View style={styles.header}>
-                <Text style={styles.cardType}>{cardType}</Text>
+                <Text style={styles.cardType}>{cardType.toUpperCase()}</Text>
                 <Text style={styles.expiryDate}>{expiryDate}</Text>
             </View>
+            <Image source={CCInner} style={{borderRadius:5, width:40, height:30}}/>
             <Text style={styles.cardNumber}>{cardNumber}</Text>
             <View style={styles.body}>
                 <Text style={styles.name}>{name}</Text>
@@ -23,10 +25,10 @@ function WalletCard({index,name,cardNumber,cardType,expiryDate,balance}) {
 
 const styles = StyleSheet.create({
     container: {
-        borderRadius: 16,
+        borderRadius: 10,
         padding: 16,
-        width: 280,
-        height: 180,
+        width: 350,
+        height: 220,
         marginHorizontal: 8
       },
       header: {
@@ -42,25 +44,25 @@ const styles = StyleSheet.create({
       cardType: {
         fontSize: 20,
         fontWeight: 'bold',
-        color: '#000'
+        color: '#ffffff',
       },
       cardNumber: {
-        fontSize: 25,
-        color: '#000'
+        fontSize: 35,
+        color: '#ffffff'
       },
       expiryDate: {
         fontSize: 16,
-        color: '#000'
+        color: '#ffffff'
       },
       name: {
         fontSize: 20,
         fontWeight: 'bold',
-        color: '#000',
+        color: '#ffffff',
         marginBottom: 8
       },
       balance: {
         fontSize: 16,
-        color: '#000'
+        color: '#ffffff'
       }
 })
 
